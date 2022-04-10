c80479b4cfa6b482c9df5286ce2a4e6ed21440f3
Vladislav <91915428+UniverTime@users.noreply.github.com>
2022-04-10 19:23:53 +0300

    add new version

---
+++ include/config.h
@@ -0,0 +1,13 @@
+
+#ifndef AKIN_CONFIG_
+
+#define AKIN_CONFIG_
+
+//===============================================
+
+const size_t INDENT_STEP    =  4,
+             WORLD_LENGTH   = 20;
+
+const size_t INT_DOT_VARIABLE_PRIOR = 9;
+
+#endif // AKIN_CONFIG_
---
+++ main.cpp
@@ -0,0 +1,10 @@
+
+
+#include "include/differentiator.h"
+
+int main(void)
+{
+    differentiator();   //int n_args, char *p_args[]
+
+    return 0;
+}
